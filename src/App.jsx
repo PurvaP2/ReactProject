2e693401d13e721bc03547a1625a81b82e6e3056
PurvaP2 <purvaparab0221@gmail.com>
2024-07-09 16:46:05 +0530

    package

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 
-
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx'
-
-import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Navigate from './pages/Navigate.jsx';
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navigate from "./pages/Navigate.jsx";
 
 const router = createBrowserRouter([
-  {path:'/', element:<Home />},
-  {path:'/about', element:<About />},
-  {path:'/navigate', element: <Navigate />}
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/navigate", element: <Navigate /> },
 ]);
 
-
-
-
- function App() {
-  return (<>
-    <RouterProvider router={router} />
-    
-   </>
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} basename={process.env.PUBLIC_URL} />
+    </>
   );
 }
 export default App;
